b7a5ab7f6c9bb3ab1a04701437be216b8b5338ad
Daniel Fox <danudey@me.com>
2026-04-16 14:32:45 -0700

    go fix/fmt/vet

--- pkg/modiff/modiff_internal_test.go
+++ pkg/modiff/modiff_internal_test.go
@@ -151,7 +151,6 @@ func TestGitHubCompareLinkTo(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			g := NewGomegaWithT(t)
--- pkg/modiff/modiff_test.go
+++ pkg/modiff/modiff_test.go
@@ -190,7 +190,6 @@ func TestCheckURLValid(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			g := NewGomegaWithT(t)
